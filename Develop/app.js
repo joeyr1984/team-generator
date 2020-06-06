d06366c45482104ec843319d1c08c757916c4e79
Joey Reyes <joeyr1984@gmail.com>
2020-06-06 15:11:54 -0700

    added questions to app js

--- Develop/app.js
+++ Develop/app.js
@@ -10,10 +10,103 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+var engineeringTeam = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
+const rolePrompt = [
+    {
+        type: "list",
+        name: "employee",
+        message: "Please select an employee role to add.",
+        choices: ["Manager", "Engineer", "Intern"]
+    }
+];
+const engineerPrompt = [
+    {
+        type: "input",
+        name: "engineerName",
+        message: "Please enter Engineers Name.",
+        default: "Bill Nye"
+    },
+    {
+        type: "input",
+        name: "engineerId",
+        message: "Please enter Engineers ID.",
+        default: "2"
+    },
+    {
+        type: "input",
+        name: "engineerEmail",
+        message: "Please enter Engineers E-mail.",
+        default: "Billnye@gmail.com"
+    },
+    {
+        type: "input",
+        name: "engineerGithub",
+        message: "Please enter Engineers Github.",
+        default: "BillNye1984"
+    }
+];
+const internPrompt  = [
+    {
+        type: "input",
+        name: "internName",
+        message: "Please enter the Interns Name.",
+        default: "Bill Nye"
+    },
+    {
+        type: "input",
+        name: "internId",
+        message: "Please enter the Intern's ID.",
+        default: "2"
+    },
+    {
+        type: "input",
+        name: "internEmail",
+        message: "Please enter the Intern'ss E-mail.",
+        default: "Billnye@gmail.com"
+    },
+    {
+        type: "input",
+        name: "internSchool",
+        message: "Please enter the Interns School.",
+        default: "Cooley High"
+    }
+];
+const managerPrompt  = [
+    {
+        type: "input",
+        name: "managerName",
+        message: "Please enter the Manager's Name.",
+        default: "Bill Nye"
+    },
+    {
+        type: "input",
+        name: "managerId",
+        message: "Please enter the Manager's ID.",
+        default: "2"
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "Please enter the Manager's E-mail.",
+        default: "Billnye@gmail.com"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter the Manager's Office Number.",
+        default: "40"
+    }
+];
+function init() {
+    inquirer.prompt(rolePrompt).then(answers => {
+    if (condition) {
+        
+    }
+    })
+}
+init();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
