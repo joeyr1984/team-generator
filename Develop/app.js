6d965be3a79976d73501e0d3e8c901dc83c3a45a
Joey Reyes <joeyr1984@gmail.com>
2020-06-07 14:06:30 -0700

    generating and exporting team html

--- Develop/app.js
+++ Develop/app.js
@@ -113,8 +113,10 @@ function teamPrompts() {
                 engineeringTeam.push(newEmployee);
                 teamPrompts();
             });
-        }else {
-            console.log(engineeringTeam);
+        } else {
+            //console.log(engineeringTeam);
+            var teamHtml = render(engineeringTeam);
+            writeToFile(OUTPUT_DIR, outputPath, teamHtml);
         }
     });
 }
@@ -126,11 +128,25 @@ function init() {
     });
 
 }
+function writeToFile(directory, fileName, data) {
+     if (!fs.existsSync(directory)) {
+       fs.mkdirSync(directory);
+           }
+    fs.writeFile(fileName, data, function(err){
+        if(err){
+            return console.log(err);
+            
+        }
+    });
+}
 init();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
